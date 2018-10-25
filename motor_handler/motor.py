a56f9ed996ad717cc30bdd8d315910a6370ac68f
spencercornish <SpencerCornish@users.noreply.github.com>
2018-10-25 17:32:23 -0600

    Add basic motor files

--- motor_handler/motor.py
+++ motor_handler/motor.py
@@ -1,23 +1,69 @@
 import pigpio
 import pinout
+import redis
 from time import sleep
+
+
+# global variables
+isConnected = False
+frontSpeed = 1000
+rearSpeed = 1000
+
+
+def emergency_stop():
+    pi.set_servo_pulsewidth(pinout.FRONT_MOTOR, 0)
+    pi.set_servo_pulsewidth(pinout.REAR_MOTOR, 0)
+    pi.set_servo_pulsewidth(pinout.FRONT_STEERING, 0)
+    pi.set_servo_pulsewidth(pinout.REAR_STEERING, 0)
+    pi.stop()
+
+
+def arm():
+    # Arm the ESCs
+    pi.set_servo_pulsewidth(pinout.FRONT_MOTOR, 0)
+    pi.set_servo_pulsewidth(pinout.REAR_MOTOR, 0)
+    sleep(.1)
+    pi.set_servo_pulsewidth(pinout.FRONT_MOTOR, 2000)
+    pi.set_servo_pulsewidth(pinout.REAR_MOTOR, 2000)
+    sleep(.1)
+    pi.set_servo_pulsewidth(pinout.FRONT_MOTOR, 1000)
+    pi.set_servo_pulsewidth(pinout.REAR_MOTOR, 1000)
+    sleep(.1)
+    # Zero the servos
+    pi.set_servo_pulsewidth(pinout.FRONT_STEERING, 700)
+    pi.set_servo_pulsewidth(pinout.FRONT_STEERING, 700)
+    sleep(.1)
+
 
 # connect to the
 pi = pigpio.pi()
-# loop forever
-while True:
 
-    pi.set_servo_pulsewidth(pinout.FRONT_STEERING, 0)    # off
-    pi.set_servo_pulsewidth(pinout.REAR_STEERING, 0)    # off
-    sleep(1)
-    # position anti-clockwise
-    pi.set_servo_pulsewidth(pinout.FRONT_STEERING, 600)
-    # position anti-clockwise
-    pi.set_servo_pulsewidth(pinout.REAR_STEERING, 600)
-    sleep(1)
-    pi.set_servo_pulsewidth(pinout.FRONT_STEERING, 1500)  # middle
-    pi.set_servo_pulsewidth(pinout.REAR_STEERING, 1500)  # middle
-    sleep(1)
-    pi.set_servo_pulsewidth(pinout.FRONT_STEERING, 2300)  # position clockwise
-    pi.set_servo_pulsewidth(pinout.REAR_STEERING, 2300)  # position clockwise
-    sleep(1)
+
+redisClient = redis.StrictRedis()
+
+
+while True:
+    # 100 Hz data refresh rate
+    sleep(.01)
+    # if we're connected but we think we aren't update.
+    if redisClient.exists("tele.heartbeat"):
+        if not isConnected:
+            isConnected = True
+            print("reconnection event. isConnected changing to True")
+    # if we're disconnected but we think we are update
+    else:
+        if isConnected:
+            isConnected = False
+            emergency_stop()
+            print("disconnection event. isConnected changing to False")
+
+    if isConnected:
+        frontMotorSpeed = redisClient.get("move.speed.front")
+        rearMotorSpeed = redisClient.get("move.speed.rear")
+        # If we have a new front speed from the app
+        if frontMotorSpeed != frontSpeed:
+            frontSpeed = frontMotorSpeed
+            pi.set_servo_pulsewidth(pinout.FRONT_MOTOR, frontSpeed)
+        if rearMotorSpeed != rearSpeed:
+            rearSpeed = rearMotorSpeed
+            pi.set_servo_pulsewidth(pinout.REAR_MOTOR, rearMotorSpeed)
