e6ac8e664a83443614e2d32a377dbd1a72c1b45d
spencercornish <SpencerCornish@users.noreply.github.com>
2018-10-28 23:55:56 -0600

    Working temps

--- gyro_handler/gyro.py
+++ gyro_handler/gyro.py
@@ -1,3 +1,18 @@
 
+from mpu6050 import mpu6050
+import redis
+from time import sleep
 
-class Main:
+
+redisClient = redis.StrictRedis()
+
+sensor = mpu6050(0x68)
+
+
+while True:
+    gyro_dict = sensor.get_gyro_data()
+    redisClient.set("tele.gyro.x", gyro_dict["x"])
+    redisClient.set("tele.gyro.y", gyro_dict["y"])
+    redisClient.set("tele.gyro.z", gyro_dict["z"])
+    redisClient.set("tele.gyro.z", sensor.get_temp())
+    sleep(1)
