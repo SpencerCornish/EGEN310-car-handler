eac7f43e43081919274c614b8de117bc0c014146
spencercornish <SpencerCornish@users.noreply.github.com>
2018-10-29 00:07:36 -0600

    Fix Accel typos

--- gyro_handler/gyro.py
+++ gyro_handler/gyro.py
@@ -10,9 +10,9 @@ sensor = mpu6050(0x68)
 
 
 while True:
-    gyro_dict = sensor.get_gyro_data()
+    gyro_dict = sensor.get_accel_data()
     redisClient.set("tele.gyro.x", gyro_dict["x"])
     redisClient.set("tele.gyro.y", gyro_dict["y"])
     redisClient.set("tele.gyro.z", gyro_dict["z"])
-    redisClient.set("tele.gyro.z", sensor.get_temp())
+    redisClient.set("tele.temp", sensor.get_temp())
     sleep(1)
